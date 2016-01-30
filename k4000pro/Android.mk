b519b2cb752f1e1777fa14f070964db24d901bd6
MrColdbird <gentleman.trilby@gmail.com>
2016-01-30 19:36:01 +0100

    Google -> Oukitel, Sprout -> K4000PRO

--- k4000pro/Android.mk
+++ k4000pro/Android.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,4 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/google/sprout/sprout-vendor-blobs.mk)
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter k4000pro, $(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_OWNER := oukitel
+LOCAL_SRC_FILES := proprietary/lib/libaudiopolicymanager.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+endif
