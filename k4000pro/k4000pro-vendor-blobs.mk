5e4adf18a59d850a408f2d6cfba97cc6d4cbddd6
MrColdbird <gentleman.trilby@gmail.com>
2016-02-03 21:10:33 +0100

    Added another batch of missing blobs

--- k4000pro/k4000pro-vendor-blobs.mk
+++ k4000pro/k4000pro-vendor-blobs.mk
@@ -32,11 +32,6 @@ PRODUCT_COPY_FILES += \
     vendor/oukitel/k4000pro/proprietary/bin/thermal_manager:system/bin/thermal_manager \
     vendor/oukitel/k4000pro/proprietary/bin/wmt_loader:system/bin/wmt_loader \
     vendor/oukitel/k4000pro/proprietary/bin/wpa_supplicant:system/bin/wpa_supplicant \
-    vendor/oukitel/k4000pro/proprietary/etc/firmware/ROMv2_lm_patch_1_0_hdr.bin:system/etc/firmware/ROMv2_lm_patch_1_0_hdr.bin \
-    vendor/oukitel/k4000pro/proprietary/etc/firmware/ROMv2_lm_patch_1_1_hdr.bin:system/etc/firmware/ROMv2_lm_patch_1_1_hdr.bin \
-    vendor/oukitel/k4000pro/proprietary/etc/firmware/S_ANDRO_SFL.ini:system/etc/firmware/S_ANDRO_SFL.ini \
-    vendor/oukitel/k4000pro/proprietary/etc/firmware/WIFI_RAM_CODE_6735:system/etc/firmware/WIFI_RAM_CODE_6735 \
-    vendor/oukitel/k4000pro/proprietary/etc/firmware/WMT_SOC.cfg:system/etc/firmware/WMT_SOC.cfg \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/catcher_filter_1_lwg_n.bin:system/etc/firmware/catcher_filter_1_lwg_n.bin \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/dsp_1_lwg_n.bin:system/etc/firmware/dsp_1_lwg_n.bin \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/modem_1_lwg_n.img:system/etc/firmware/modem_1_lwg_n.img \
@@ -50,6 +45,11 @@ PRODUCT_COPY_FILES += \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/mt6627/mt6627_fm_v4_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v4_patch.bin \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/mt6627/mt6627_fm_v5_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v5_coeff.bin \
     vendor/oukitel/k4000pro/proprietary/etc/firmware/mt6627/mt6627_fm_v5_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v5_patch.bin \
+    vendor/oukitel/k4000pro/proprietary/etc/firmware/ROMv2_lm_patch_1_0_hdr.bin:system/etc/firmware/ROMv2_lm_patch_1_0_hdr.bin \
+    vendor/oukitel/k4000pro/proprietary/etc/firmware/ROMv2_lm_patch_1_1_hdr.bin:system/etc/firmware/ROMv2_lm_patch_1_1_hdr.bin \
+    vendor/oukitel/k4000pro/proprietary/etc/firmware/S_ANDRO_SFL.ini:system/etc/firmware/S_ANDRO_SFL.ini \
+    vendor/oukitel/k4000pro/proprietary/etc/firmware/WIFI_RAM_CODE_6735:system/etc/firmware/WIFI_RAM_CODE_6735 \
+    vendor/oukitel/k4000pro/proprietary/etc/firmware/WMT_SOC.cfg:system/etc/firmware/WMT_SOC.cfg \
     vendor/oukitel/k4000pro/proprietary/etc/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg \
     vendor/oukitel/k4000pro/proprietary/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     vendor/oukitel/k4000pro/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
@@ -65,10 +65,6 @@ PRODUCT_COPY_FILES += \
     vendor/oukitel/k4000pro/proprietary/lib/hw/lights.default.so:system/lib/hw/lights.default.so \
     vendor/oukitel/k4000pro/proprietary/lib/hw/sensors.mt6735m.so:system/lib/hw/sensors.mt6735m.so \
     vendor/oukitel/k4000pro/proprietary/lib/lib3a.so:system/lib/lib3a.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libJpgEncPipe.so:system/lib/libJpgEncPipe.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxCore.so:system/lib/libMtkOmxCore.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxVdec.so:system/lib/libMtkOmxVdec.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxVenc.so:system/lib/libMtkOmxVenc.so \
     vendor/oukitel/k4000pro/proprietary/lib/libaed.so:system/lib/libaed.so \
     vendor/oukitel/k4000pro/proprietary/lib/libaudiocompensationfilter.so:system/lib/libaudiocompensationfilter.so \
     vendor/oukitel/k4000pro/proprietary/lib/libaudiocomponentengine.so:system/lib/libaudiocomponentengine.so \
@@ -79,47 +75,51 @@ PRODUCT_COPY_FILES += \
     vendor/oukitel/k4000pro/proprietary/lib/libaudiopolicymanager.so:system/lib/libaudiopolicymanager.so \
     vendor/oukitel/k4000pro/proprietary/lib/libaudiosetting.so:system/lib/libaudiosetting.so \
     vendor/oukitel/k4000pro/proprietary/lib/libbessound_hd_mtk.so:system/lib/libbessound_hd_mtk.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libblisrc.so:system/lib/libblisrc.so \
     vendor/oukitel/k4000pro/proprietary/lib/libblisrc32.so:system/lib/libblisrc32.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libblisrc.so:system/lib/libblisrc.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libbluetoothdrv.so:system/lib/libbluetoothdrv.so \
     vendor/oukitel/k4000pro/proprietary/lib/libbluetooth_mtk.so:system/lib/libbluetooth_mtk.so \
     vendor/oukitel/k4000pro/proprietary/lib/libbwc.so:system/lib/libbwc.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam1_utils.so:system/lib/libcam1_utils.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam3_app.so:system/lib/libcam3_app.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam3_hwnode.so:system/lib/libcam3_hwnode.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam3_hwpipeline.so:system/lib/libcam3_hwpipeline.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam3_pipeline.so:system/lib/libcam3_pipeline.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam3_utils.so:system/lib/libcam3_utils.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcamalgo.so:system/lib/libcamalgo.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.camadapter.so:system/lib/libcam.camadapter.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.campipe.so:system/lib/libcam.campipe.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.camshot.so:system/lib/libcam.camshot.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.client.so:system/lib/libcam.client.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.device1.so:system/lib/libcam.device1.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.device3.so:system/lib/libcam.device3.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcamdrv_FrmB.so:system/lib/libcamdrv_FrmB.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcamdrv.so:system/lib/libcamdrv.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcamera_client_mtk.so:system/lib/libcamera_client_mtk.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcameracustom.so:system/lib/libcameracustom.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.exif.so:system/lib/libcam.exif.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.exif.v3.so:system/lib/libcam.exif.v3.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.hal3a.v3.so:system/lib/libcam.hal3a.v3.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam.halsensor.so:system/lib/libcam.halsensor.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.iopipe.so:system/lib/libcam.iopipe.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.iopipe_FrmB.so:system/lib/libcam.iopipe_FrmB.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.metadata.so:system/lib/libcam.metadata.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.metadataprovider.so:system/lib/libcam.metadataprovider.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.paramsmgr.so:system/lib/libcam.paramsmgr.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.utils.sensorlistener.so:system/lib/libcam.utils.sensorlistener.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam.utils.so:system/lib/libcam.utils.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam3_app.so:system/lib/libcam3_app.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam3_hwnode.so:system/lib/libcam3_hwnode.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam3_hwpipeline.so:system/lib/libcam3_hwpipeline.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam3_pipeline.so:system/lib/libcam3_pipeline.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcam3_utils.so:system/lib/libcam3_utils.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam_hwutils.so:system/lib/libcam_hwutils.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.iopipe_FrmB.so:system/lib/libcam.iopipe_FrmB.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.iopipe.so:system/lib/libcam.iopipe.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.metadataprovider.so:system/lib/libcam.metadataprovider.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.metadata.so:system/lib/libcam.metadata.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam_mmp.so:system/lib/libcam_mmp.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.paramsmgr.so:system/lib/libcam.paramsmgr.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam_platform.so:system/lib/libcam_platform.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.utils.sensorlistener.so:system/lib/libcam.utils.sensorlistener.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcam_utils.so:system/lib/libcam_utils.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcamalgo.so:system/lib/libcamalgo.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcamdrv.so:system/lib/libcamdrv.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcamdrv_FrmB.so:system/lib/libcamdrv_FrmB.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcamera_client_mtk.so:system/lib/libcamera_client_mtk.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libcameracustom.so:system/lib/libcameracustom.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libcam.utils.so:system/lib/libcam.utils.so \
     vendor/oukitel/k4000pro/proprietary/lib/libccci_util.so:system/lib/libccci_util.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcurl.so:system/lib/libcurl.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcustom_nvram.so:system/lib/libcustom_nvram.so \
     vendor/oukitel/k4000pro/proprietary/lib/libcvsd_mtk.so:system/lib/libcvsd_mtk.so \
     vendor/oukitel/k4000pro/proprietary/lib/libdpframework.so:system/lib/libdpframework.so \
     vendor/oukitel/k4000pro/proprietary/lib/libdrmmtkutil.so:system/lib/libdrmmtkutil.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libdrmmtkwhitelist.so:system/lib/libdrmmtkwhitelist.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libfeatureiodrv.so:system/lib/libfeatureiodrv.so \
     vendor/oukitel/k4000pro/proprietary/lib/libfeatureio.so:system/lib/libfeatureio.so \
     vendor/oukitel/k4000pro/proprietary/lib/libfile_op.so:system/lib/libfile_op.so \
     vendor/oukitel/k4000pro/proprietary/lib/libfmcust.so:system/lib/libfmcust.so \
@@ -127,39 +127,60 @@ PRODUCT_COPY_FILES += \
     vendor/oukitel/k4000pro/proprietary/lib/libged.so:system/lib/libged.so \
     vendor/oukitel/k4000pro/proprietary/lib/libgpu_aux.so:system/lib/libgpu_aux.so \
     vendor/oukitel/k4000pro/proprietary/lib/libgralloc_extra.so:system/lib/libgralloc_extra.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libgui_ext.so:system/lib/libgui_ext.so \
     vendor/oukitel/k4000pro/proprietary/lib/libhwm.so:system/lib/libhwm.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libimageio.so:system/lib/libimageio.so \
     vendor/oukitel/k4000pro/proprietary/lib/libimageio_FrmB.so:system/lib/libimageio_FrmB.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libimageio_plat_drv.so:system/lib/libimageio_plat_drv.so \
     vendor/oukitel/k4000pro/proprietary/lib/libimageio_plat_drv_FrmB.so:system/lib/libimageio_plat_drv_FrmB.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libion.so:system/lib/libion.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libimageio_plat_drv.so:system/lib/libimageio_plat_drv.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libimageio.so:system/lib/libimageio.so \
     vendor/oukitel/k4000pro/proprietary/lib/libion_mtk.so:system/lib/libion_mtk.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libion.so:system/lib/libion.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libJpgEncPipe.so:system/lib/libJpgEncPipe.so \
     vendor/oukitel/k4000pro/proprietary/lib/libm4u.so:system/lib/libm4u.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmhalImageCodec.so:system/lib/libmhalImageCodec.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmmprofile.so:system/lib/libmmprofile.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmmsdkservice.so:system/lib/libmmsdkservice.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmnl.so:system/lib/libmnl.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmp4enc_sa.ca7.so:system/lib/libmp4enc_sa.ca7.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmpoencoder.so:system/lib/libmpoencoder.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmpo.so:system/lib/libmpo.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmsbc_mtk.so:system/lib/libmsbc_mtk.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmtcloader.so:system/lib/libmtcloader.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmtk_drvb.so:system/lib/libmtk_drvb.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmtkjpeg.so:system/lib/libmtkjpeg.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmtklimiter.so:system/lib/libmtklimiter.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmtk_mali_user.so:system/lib/libmtk_mali_user.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmtk_mmutils.so:system/lib/libmtk_mmutils.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libmtkjpeg.so:system/lib/libmtkjpeg.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxCore.so:system/lib/libMtkOmxCore.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxVdec.so:system/lib/libMtkOmxVdec.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libMtkOmxVenc.so:system/lib/libMtkOmxVenc.so \
     vendor/oukitel/k4000pro/proprietary/lib/libmtkplayer.so:system/lib/libmtkplayer.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libnvram.so:system/lib/libnvram.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libmtkshifter.so:system/lib/libmtkshifter.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libnvramagentclient.so:system/lib/libnvramagentclient.so \
     vendor/oukitel/k4000pro/proprietary/lib/libnvram_daemon_callback.so:system/lib/libnvram_daemon_callback.so \
     vendor/oukitel/k4000pro/proprietary/lib/libnvram_platform.so:system/lib/libnvram_platform.so \
     vendor/oukitel/k4000pro/proprietary/lib/libnvram_sec.so:system/lib/libnvram_sec.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libnvram.so:system/lib/libnvram.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libperfservicenative.so:system/lib/libperfservicenative.so \
     vendor/oukitel/k4000pro/proprietary/lib/libpq_prot.so:system/lib/libpq_prot.so \
     vendor/oukitel/k4000pro/proprietary/lib/librilmtk.so:system/lib/librilmtk.so \
     vendor/oukitel/k4000pro/proprietary/lib/libspeech_enh_lib.so:system/lib/libspeech_enh_lib.so \
     vendor/oukitel/k4000pro/proprietary/lib/libssladp.so:system/lib/libssladp.so \
     vendor/oukitel/k4000pro/proprietary/lib/libstagefrighthw.so:system/lib/libstagefrighthw.so \
     vendor/oukitel/k4000pro/proprietary/lib/libstlport.so:system/lib/libstlport.so \
-    vendor/oukitel/k4000pro/proprietary/lib/libvcodec_utility.so:system/lib/libvcodec_utility.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libtinycompress.so:system/lib/libtinycompress.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libui_ext.so:system/lib/libui_ext.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libvc1dec_sa.ca7.so:system/lib/libvc1dec_sa.ca7.so \
     vendor/oukitel/k4000pro/proprietary/lib/libvcodecdrv.so:system/lib/libvcodecdrv.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libvcodec_oal.so:system/lib/libvcodec_oal.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libvcodec_utility.so:system/lib/libvcodec_utility.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libvp8dec_sa.ca7.so:system/lib/libvp8dec_sa.ca7.so \
+    vendor/oukitel/k4000pro/proprietary/lib/libwapi.so:system/lib/libwapi.so \
     vendor/oukitel/k4000pro/proprietary/lib/mtk-ril.so:system/lib/mtk-ril.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-    vendor/oukitel/k4000pro/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/oukitel/k4000pro/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/mediadrm/libmockdrmcryptoplugin.so:system/vendor/lib/mediadrm/libmockdrmcryptoplugin.so \
     vendor/oukitel/k4000pro/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
